90eea5abfd715ef982c07b15213af6467debe1c6
kochevRisto <risto.kochev@miraclemill.com>
2020-06-07 12:08:09 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import "./styles.css";
-document.getElementById("app").innerHTML = "<h1>Testing</h1>";
-// ================================================================
 
 console.log("testing");
